cda6d4344fb4d9bed56034178145017631f5a916
al23077 <al23077@shibaura-it.ac.jp>
2025-06-24 17:25:58 +0900

    UserInfoEditを追加

--- frontend/src/routes/UserRoutes.jsx
+++ frontend/src/routes/UserRoutes.jsx
@@ -1,9 +1,10 @@
 // M3 ユーザUI主処理に対応する。 User関連のルーティングを行う
 
 import { Route } from 'react-router-dom';
+import UserInfoEditPage from '../pages/user_info_edit_page';
 
 export const UserRoutes = (
   <>
-
+    <Route path="/user/:userId/edit" element={<UserInfoEditPage />} />
   </>
 )
