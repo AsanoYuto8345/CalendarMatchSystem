fced566226d93409fb7bd44c08bd1123e6a28a63
al23077 <al23077@shibaura-it.ac.jp>
2025-06-24 17:10:33 +0900

    CalendarViewを追加

--- frontend/src/routes/CommunityRoutes.jsx
+++ frontend/src/routes/CommunityRoutes.jsx
@@ -2,9 +2,11 @@
 
 import { Route } from 'react-router-dom';
 import CommunityLeavePage from '../pages/community_leave_page' 
+import CommunityCalendarViewPage from '../pages/community_calendar_view_page';
 
 export const CommunityRoutes = (
   <>
-    <Route path="community/:communityId/leave" element={<CommunityLeavePage />} />
+    <Route path="community/:communityId/leave" element={<CommunityLeavePage />}/>
+    <Route path="community/:communityId/calendar/view" element={<CommunityCalendarViewPage/>}/>
   </>
 )
