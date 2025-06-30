a8e77719c552232df92b534e2a688264c2baa3dc
Dinja8345 <pocchi8345@gmail.com>
2025-06-30 18:52:50 +0900

    命名規則に沿ってTagモデルの変更

--- backend/database/models/tag.py
+++ backend/database/models/tag.py
@@ -1,3 +1,5 @@
+# Tagのデータベースモデルを定義するclass 作成者: 浅野 勇翔
+
 from app import db
 
 class Tag(db.Model):
@@ -9,10 +11,10 @@ class Tag(db.Model):
     # __bind_key__ は指定しない (デフォルトのDBを使用するため)
     id = db.Column(db.String(50), unique=True, primary_key=True) # タグID
     name = db.Column(db.String(100), nullable=False) # 表示名
-    tag_color = db.Column(db.String(6), nullable=False) # タグのカラーコード
-    submiter_id = db.Column(db.String(100), nullable=False) # 
-    community_id = db.Column(db.String(100), nullable=False)
-    date =  db.Column(db.Date, nullable=False)
+    color = db.Column(db.String(6), nullable=False) # タグのカラーコード
+    submitter_id = db.Column(db.String(100), nullable=False) # タグ登録者のID
+    community_id = db.Column(db.String(100), nullable=False) # コミュニティID
+    date =  db.Column(db.Date, nullable=False) # タグの対象日付け
 
     def __repr__(self):
         return f"<Tag(id='{self.id}', name='{self.name}')>"
