398bad04f19786e3253aba8b7d8eaf7924bcf305
Dinja8345 <pocchi8345@gmail.com>
2025-07-01 13:49:27 +0900

    途中保存

---
+++ backend/modules/matching/matching.py
@@ -0,0 +1,19 @@
+# C6マッチング処理部の機能が実装されたMatchingクラスを定義するプログラム 作成者: 浅野勇翔
+
+
+class Matching:
+    """
+    C6 マッチング処理部
+    """
+    
+    def __init__(self):
+        """
+        コンストラクタ
+        """
+        
+        
+    def find_matching(self, tag_id, commutniy_id, tag_name, date, id):
+        """
+            
+        """        
+        
