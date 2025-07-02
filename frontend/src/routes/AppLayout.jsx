05fde4d2e481a17d0014a73ce266e96f9cb80e94
PC_User <endo8800a>
2025-07-02 15:58:09 +0900

    これ間違えてつくってプッシュしたから空のやつプッシュする

--- frontend/src/routes/AppLayout.jsx
+++ frontend/src/routes/AppLayout.jsx
@@ -1,34 +1,34 @@
-// frontend/src/layouts/AppLayout.jsx
+// // frontend/src/routes/AppLayout.jsx
 
-import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
-import Cookies from "js-cookie";
-import axios from "axios";
-import HambergerMenuUI from "../components/HambergerMenuUI";
+// import { useEffect, useState } from "react";
+// import { Outlet } from "react-router-dom";
+// import Cookies from "js-cookie";
+// import axios from "axios";
+// import HambergerMenuUI from "../components/HambergerMenuUI";
 
-export default function AppLayout() {
-    const [joinedCommunities, setJoinedCommunities] = useState([]);
+// export default function AppLayout() {
+//     const [joinedCommunities, setJoinedCommunities] = useState([]);
 
-    useEffect(() => {
-    const userId = Cookies.get("userId");
-    if (!userId) return;
+//     useEffect(() => {
+//     const userId = Cookies.get("userId");
+//     if (!userId) return;
 
-    axios
-        .get(`${process.env.REACT_APP_API_SERVER_URL}/api/community/joined`, {
-            params: { user_id: userId },
-        })
-        .then((res) => {
-            setJoinedCommunities(res.data.communities || []);
-        })
-        .catch((err) => {
-            console.error("所属コミュニティ取得失敗:", err);
-        });
-    }, []);
+//     axios
+//         .get(`${process.env.REACT_APP_API_SERVER_URL}/api/community/joined`, {
+//             params: { user_id: userId },
+//         })
+//         .then((res) => {
+//             setJoinedCommunities(res.data.communities || []);
+//         })
+//         .catch((err) => {
+//             console.error("所属コミュニティ取得失敗:", err);
+//         });
+//     }, []);
 
-    return (
-    <>
-        <HambergerMenuUI communities={joinedCommunities} />
-        <Outlet />
-    </>
-    );
-}
+//     return (
+//     <>
+//         <HambergerMenuUI communities={joinedCommunities} />
+//         <Outlet />
+//     </>
+//     );
+// }
